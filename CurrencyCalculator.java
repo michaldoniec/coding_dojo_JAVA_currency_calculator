68de2e700498f993bc4eff7462093c9385c5600f
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-04-12 10:11:13 +0200

    Calculator problem with type of amount

--- CurrencyCalculator.java
+++ CurrencyCalculator.java
@@ -5,12 +5,12 @@ import org.json.*;
 public class CurrencyCalculator {
 
   public static void main(String[] args) {
-  System.out.println(loadFile());
+
+  System.out.println(Calculator(loadFile(),"PHP", 2.0, "PLN"));
   }
 
   public static JSONObject loadFile() {
     JSONObject numbers;
-
     try {
           numbers = new JSONObject(new FileReader("money.json"));
           FileReader currency = new FileReader("money.json");
@@ -24,14 +24,26 @@ public class CurrencyCalculator {
         numbers = new JSONObject("....");
         return numbers;
     }
+  }
 
+  public static Double Calculator (JSONObject currencyData, String currencyChosen, Double amount, String currentCurrency) {
+    JSONObject currencies = currencyData.getJSONObject("rates");
+    Double basevalue;
+    Double sum;
+    System.out.println(currencies.get(currencyChosen).getClass());
+    if (currentCurrency == currencyData.get("base")){
+     basevalue = amount;
+    }
+    else{
+      Double x = new Double(currencies.get(currentCurrency));
+     basevalue = amount * x;
+    }
+    Double currency2 = new Double(currencies.get(currencyChosen));
+
+    sum = basevalue * currency2;
+    return sum;
 
   }
 
-  // public static Integer Calculator {
-  //
-  //
-  //
-  // }
 
 }
